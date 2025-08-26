ef9be2ad2ba7d4c39a16de07e251219f7c49d467
mrgrose <grachev.ro@gmail.com>
2025-08-26 14:50:27 +0300

    Setup logging

--- write_chat.py
+++ write_chat.py
@@ -6,19 +6,27 @@ import configargparse
 logger = logging.getLogger(__name__)
 
 
-async def wtire_chat(host, port, account_hash):
+async def write_chat(host, port, account_hash, message):
     reader, writer = await asyncio.open_connection(host, port)
-    message = "Я снова тестирую чатик. Это третье сообщение."
 
-    writer.write(account_hash.encode() + b"\n")
-    await writer.drain()
+    try:
+        data = await reader.read(150)
+        logger.debug(data.decode().strip())
 
-    for _ in range(3):
+        logger.debug(f"user account_hash: {account_hash}")
+        writer.write(account_hash.encode() + b"\n")
+        await writer.drain()
+
+        data = await reader.readline()
+        logger.debug(data.decode().strip())
+
+        logger.debug(f"user message: {message}")
         writer.write(message.encode() + b"\n\n")
         await writer.drain()
-        await asyncio.sleep(3)
-    writer.close()
-    await writer.wait_closed()
+
+    finally:
+        writer.close()
+        await writer.wait_closed()
 
 
 def create_parser():
@@ -26,16 +34,17 @@ def create_parser():
     parser.add("-c", "--config", is_config_file=True, help="Путь к конфиг. файлу")
     parser.add("--host", env_var="HOST", help="host сервера")
     parser.add("--port", env_var="WRITE_PORT", help="port сервера")
-    parser.add("--hash", env_var="CHAT_TOKEN", help="token пользователя")
+    parser.add("--hash", env_var="USER_TOKEN", help="token пользователя")
+    parser.add("-m", default="Я снова тестирую чатик. Раз-два-три.", help="сообщение")
     return parser
 
 
 async def main():
-    logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+    logging.basicConfig(filename="write_chat.log", format="%(asctime)s - %(levelname)s - %(message)s", level=logging.DEBUG)
     parser = create_parser()
     args = parser.parse_args()
 
-    await wtire_chat(args.host, args.port, args.hash)
+    await write_chat(args.host, args.port, args.hash, args.m)
 
 
 if __name__ == "__main__":
