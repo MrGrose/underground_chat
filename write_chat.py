2bed4fbf6e3fd1a9ad2e3955bb9d9a7f262994bd
mrgrose <grachev.ro@gmail.com>
2025-08-27 13:55:03 +0300

    refactor code, split by reusable functions

--- write_chat.py
+++ write_chat.py
@@ -4,14 +4,16 @@ import json
 import logging
 
 import configargparse
+from dotenv import load_dotenv
 
 logger = logging.getLogger(__name__)
 
 
-async def registration(host, port):
+async def registration(host, port, nickname):
+
     reader, writer = await asyncio.open_connection(host, port)
     try:
-        data = await reader.read(150)
+        data = await reader.readline()
         logger.debug(data.decode().strip())
 
         writer.write(b"\n")
@@ -19,6 +21,10 @@ async def registration(host, port):
 
         data = await reader.readline()
         logger.debug(data.decode().strip())
+
+        if nickname:
+            writer.write(nickname.encode().strip() + b"\n")
+            await writer.drain()
 
         writer.write(b"\n")
         await writer.drain()
@@ -33,7 +39,29 @@ async def registration(host, port):
     return json.loads(data.decode().strip())
 
 
-async def write_chat(host, port, account_hash, message):
+async def authorise(host, port, account_hash):
+    reader, writer = await asyncio.open_connection(host, port)
+    try:
+        data = await reader.readline()
+        logger.debug(data.decode().strip())
+
+        writer.write(account_hash.encode() + b"\n")
+        await writer.drain()
+
+        data = await reader.readline()
+        logger.debug(data.decode().strip())
+
+        if json.loads(data.decode()) is None:
+            print("Неизвестный токен. Проверьте его или зарегистрируйте заново.")
+            return False
+
+        return True
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
+
+async def submit_message(host, port, account_hash, message):
     reader, writer = await asyncio.open_connection(host, port)
     try:
         data = await reader.readline()
@@ -45,10 +73,6 @@ async def write_chat(host, port, account_hash, message):
 
         data = await reader.readline()
         logger.debug(data.decode().strip())
-
-        if json.loads(data.decode()) is None:
-            print("Неизвестный токен. Проверьте его или зарегистрируйте заново.")
-            return
 
         logger.debug(f"user message: {message}")
         writer.write(message.encode() + b"\n\n")
@@ -65,29 +89,41 @@ def create_parser():
     parser.add("--host", env_var="HOST", help="host сервера")
     parser.add("--port", env_var="WRITE_PORT", help="port сервера")
     parser.add("--hash", env_var="USER_TOKEN", help="token пользователя")
+    parser.add("--nickname", env_var="USER_NAME", help="nickname пользователя")
     parser.add("-m", default="Я снова тестирую чатик. Раз-два-три.", help="сообщение в чат")
     return parser
 
 
-def update_hash(new_hash, filename="config_write.ini"):
+def update_config(account_hash, nickname, filename="config_write.ini"):
     config = configparser.ConfigParser()
     config.read(filename)
-    config.set("WRITE", "hash", new_hash)
+    config.set("WRITE", "hash", account_hash)
+    config.set("WRITE", "nickname", nickname)
     with open(filename, "w") as configfile:
         config.write(configfile)
 
 
 async def main():
     logging.basicConfig(filename="write_chat.log", format="%(asctime)s - %(levelname)s - %(message)s", level=logging.DEBUG)
+    load_dotenv(".env")
     parser = create_parser()
     args = parser.parse_args()
 
     if not args.hash:
-        account_hash = await registration(args.host, args.port)
-        update_hash(account_hash["account_hash"])
-        await write_chat(args.host, args.port, account_hash["account_hash"], args.m)
+        account_hash = await registration(args.host, args.port, args.nickname)
+        update_config(account_hash["account_hash"], account_hash["nickname"])
+        args = parser.parse_args()
+        await submit_message(args.host, args.port, args.hash, args.m)
 
-    await write_chat(args.host, args.port, args.hash, args.m)
+    is_authorized = await authorise(args.host, args.port, args.hash)
+
+    if not is_authorized:
+        account_hash_data = await registration(args.host, args.port, args.nickname)
+        update_config(account_hash_data["account_hash"], account_hash_data["nickname"])
+        args = parser.parse_args()
+
+    await submit_message(args.host, args.port, args.hash, args.m)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
