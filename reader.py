6e2f21caf2d539fcee45f9b0a0bf84cf9b5bec9d
mrgrose <grachev.ro@gmail.com>
2025-08-27 20:41:31 +0300

    rename files

---
+++ reader.py
@@ -0,0 +1,71 @@
+import argparse
+import asyncio
+import datetime
+import logging
+from pathlib import Path
+
+import aiofiles
+from environs import Env
+
+logger = logging.getLogger(__name__)
+
+
+async def read_chat(host, port, folder_path):
+    while True:
+        try:
+            reader, writer = await asyncio.open_connection(host, port)
+            while line_bytes := await reader.readline():
+                await save_message(line_bytes, folder_path)
+                await asyncio.sleep(1)
+            writer.close()
+            await writer.wait_closed()
+            break
+        except ConnectionError as e:
+            logger.error(f"Сетевая ошибка: {e}")
+            await asyncio.sleep(5)
+        except Exception as e:
+            logger.error(f"Ошибка: {e}")
+            await asyncio.sleep(5)
+
+
+async def save_message(line_bytes, folder_path):
+    folder = Path(__file__).parent / folder_path
+    folder.mkdir(exist_ok=True)
+    file_path = folder / "data.txt"
+    formatted_date = datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
+    message = f"[{formatted_date}] {line_bytes.decode()}"
+    async with aiofiles.open(file_path, "a", encoding="utf-8") as file:
+        await file.write(message)
+
+    print(message, end="")
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="Загрузка истории переписки из чата")
+    parser.add_argument("--host", default="minechat.dvmn.org", help="host сервера")
+    parser.add_argument("--port", default=5000, help="port сервера")
+    parser.add_argument("--history", default="minechat", help="Путь к файлу с историей переписки")
+
+    return parser
+
+
+async def main():
+    logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+    env = Env()
+    env.read_env()
+
+    parser = create_parser()
+    args = parser.parse_args()
+    host = args.host or env.str("HOST")
+    port = args.port or env.int("READ_PORT")
+    history = args.history or env.str("HISTORY")
+
+    history_path = Path(history)
+    if not history_path.is_dir():
+        history_path.mkdir(parents=True, exist_ok=True)
+
+    await read_chat(host, port, history)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ sender.py
@@ -0,0 +1,132 @@
+import argparse
+import asyncio
+import json
+import logging
+
+import aiofiles
+from environs import Env
+
+logger = logging.getLogger(__name__)
+
+
+async def registration(host, port, nickname):
+    reader, writer = await asyncio.open_connection(host, port)
+    try:
+        data = await reader.readline()
+        logger.debug(data.decode().strip())
+
+        writer.write(b"\n")
+        await writer.drain()
+
+        data = await reader.readline()
+        logger.debug(data.decode().strip())
+
+        if nickname:
+            writer.write(nickname.encode())
+            await writer.drain()
+
+        writer.write(b"\n")
+        await writer.drain()
+
+        data = await reader.readline()
+        logger.debug(f"Регистрация: {data.decode().strip()}")
+
+        async with aiofiles.open("user_auth.txt", "w") as file:
+            await file.write(data.decode().strip())
+
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
+    return json.loads(data.decode().strip())
+
+
+async def authorise(host, port, token):
+    reader, writer = await asyncio.open_connection(host, port)
+    try:
+        data = await reader.readline()
+        logger.debug(data.decode().strip())
+
+        writer.write(token.encode() + b"\n")
+        logger.debug(f"Проверка token: {token}")
+        await writer.drain()
+
+        data = await reader.readline()
+        logger.debug(f"Полученные данные: {data.decode().strip()}")
+
+        if json.loads(data.decode()) is None:
+            print("Неизвестный токен. Проверьте его или зарегистрируйте заново.")
+            return False
+        return True
+
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
+
+async def submit_message(host, port, token, message):
+    reader, writer = await asyncio.open_connection(host, port)
+    try:
+        await reader.readline()
+        writer.write(token.encode() + b"\n")
+        await writer.drain()
+
+        message_edit = "".join(message).replace("\n", "")
+        logger.debug(f"Соообщение: {message_edit}")
+        writer.write(message_edit.encode() + b"\n\n")
+        await writer.drain()
+
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="Отправка сообщений в чат")
+    parser.add_argument("--host", default="minechat.dvmn.org", help="host сервера")
+    parser.add_argument("--port", default=5050, help="port сервера")
+    parser.add_argument("--token", help="token пользователя")
+    parser.add_argument("--nickname", help="nickname пользователя")
+    parser.add_argument("-m", help="сообщение в чат")
+    return parser
+
+
+async def main():
+    logging.basicConfig(filename="chat.log", format="%(asctime)s - %(levelname)s - %(message)s", level=logging.DEBUG)
+    env = Env()
+    env.read_env()
+
+    parser = create_parser()
+    args = parser.parse_args()
+
+    host = args.host or env.str("HOST")
+    port = args.port or env.int("SEND_PORT")
+    nickname = args.nickname or env.str("USER_NAME", None)
+    token = args.token or env.str("USER_TOKEN", None)
+    try:
+        if not args.m:
+            print("Текст сообщение не передан")
+            return
+
+        if not token:
+            user_data = await registration(host, port, nickname)
+            await submit_message(host, port, user_data["account_hash"], args.m)
+
+        else:
+            is_authorized = await authorise(host, port, token)
+            if not is_authorized:
+                print("Проблема с авторизацией, проверьте данные и повторите попытку")
+                return
+
+            await submit_message(host, port, token, args.m)
+
+    except OSError as e:
+        logger.error(f"Ошибка соединения: {e}")
+    except ConnectionRefusedError:
+        logger.error("Соединение отклонено")
+    except Exception as e:
+        logger.error(f"Ошибка: {e}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
