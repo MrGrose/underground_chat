bbba349a6d0003be83a7cde519cc8495703705cd
mrgrose <grachev.ro@gmail.com>
2025-08-27 16:32:09 +0300

    scape control symbols.

--- write_chat.py
+++ write_chat.py
@@ -10,7 +10,6 @@ logger = logging.getLogger(__name__)
 
 
 async def registration(host, port, nickname):
-
     reader, writer = await asyncio.open_connection(host, port)
     try:
         data = await reader.readline()
@@ -23,14 +22,14 @@ async def registration(host, port, nickname):
         logger.debug(data.decode().strip())
 
         if nickname:
-            writer.write(nickname.encode().strip() + b"\n")
+            writer.write(nickname.encode())
             await writer.drain()
 
         writer.write(b"\n")
         await writer.drain()
 
         data = await reader.readline()
-        logger.debug(data.decode().strip())
+        logger.debug(f"Регистрация: {data.decode().strip()}")
 
     finally:
         writer.close()
@@ -39,43 +38,39 @@ async def registration(host, port, nickname):
     return json.loads(data.decode().strip())
 
 
-async def authorise(host, port, account_hash):
+async def authorise(host, port, token):
     reader, writer = await asyncio.open_connection(host, port)
     try:
         data = await reader.readline()
         logger.debug(data.decode().strip())
 
-        writer.write(account_hash.encode() + b"\n")
+        writer.write(token.encode() + b"\n")
+        logger.debug(f"Проверка token: {token}")
         await writer.drain()
 
         data = await reader.readline()
-        logger.debug(data.decode().strip())
+        logger.debug(f"Полученные данные: {data.decode().strip()}")
 
         if json.loads(data.decode()) is None:
             print("Неизвестный токен. Проверьте его или зарегистрируйте заново.")
             return False
-
         return True
+
     finally:
         writer.close()
         await writer.wait_closed()
 
 
-async def submit_message(host, port, account_hash, message):
+async def submit_message(host, port, token, message):
     reader, writer = await asyncio.open_connection(host, port)
     try:
-        data = await reader.readline()
-        logger.debug(data.decode().strip())
-
-        logger.debug(f"user account_hash: {account_hash}")
-        writer.write(account_hash.encode() + b"\n")
+        await reader.readline()
+        writer.write(token.encode() + b"\n")
         await writer.drain()
 
-        data = await reader.readline()
-        logger.debug(data.decode().strip())
-
-        logger.debug(f"user message: {message}")
-        writer.write(message.encode() + b"\n\n")
+        message_edit = "".join(message).replace("\n", "")
+        logger.debug(f"Соообщение: {message_edit}")
+        writer.write(message_edit.encode() + b"\n\n")
         await writer.drain()
 
     finally:
@@ -88,16 +83,16 @@ def create_parser():
     parser.add("-c", "--config", is_config_file=True, help="Путь к конфиг. файлу")
     parser.add("--host", env_var="HOST", help="host сервера")
     parser.add("--port", env_var="WRITE_PORT", help="port сервера")
-    parser.add("--hash", env_var="USER_TOKEN", help="token пользователя")
+    parser.add("--token", env_var="USER_TOKEN", help="token пользователя")
     parser.add("--nickname", env_var="USER_NAME", help="nickname пользователя")
     parser.add("-m", default="Я снова тестирую чатик. Раз-два-три.", help="сообщение в чат")
     return parser
 
 
-def update_config(account_hash, nickname, filename="config_write.ini"):
+def update_config(token, nickname, filename="config_write.ini"):
     config = configparser.ConfigParser()
     config.read(filename)
-    config.set("WRITE", "hash", account_hash)
+    config.set("WRITE", "token", token)
     config.set("WRITE", "nickname", nickname)
     with open(filename, "w") as configfile:
         config.write(configfile)
@@ -109,20 +104,19 @@ async def main():
     parser = create_parser()
     args = parser.parse_args()
 
-    if not args.hash:
-        account_hash = await registration(args.host, args.port, args.nickname)
-        update_config(account_hash["account_hash"], account_hash["nickname"])
+    if not args.token:
+        user_data = await registration(args.host, args.port, args.nickname)
+        update_config(user_data["account_hash"], user_data["nickname"])
         args = parser.parse_args()
-        await submit_message(args.host, args.port, args.hash, args.m)
+        await submit_message(args.host, args.port, args.token, args.m)
+    else:
+        is_authorized = await authorise(args.host, args.port, args.token)
+        if not is_authorized:
+            account_data = await registration(args.host, args.port, args.nickname)
+            update_config(account_data["account_hash"], account_data["nickname"])
+            args = parser.parse_args()
 
-    is_authorized = await authorise(args.host, args.port, args.hash)
-
-    if not is_authorized:
-        account_hash_data = await registration(args.host, args.port, args.nickname)
-        update_config(account_hash_data["account_hash"], account_hash_data["nickname"])
-        args = parser.parse_args()
-
-    await submit_message(args.host, args.port, args.hash, args.m)
+        await submit_message(args.host, args.port, args.token, args.m)
 
 
 if __name__ == "__main__":
